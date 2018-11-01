488690af8f073ff4039d4734aa8940c2be12f580
Al <>
2018-11-01 13:36:54 +0000

    added printout of results to Dice#roll and refactored all tests

--- lib/dice.rb
+++ lib/dice.rb
@@ -2,6 +2,7 @@ class Dice
   def roll(num = 1)
     results = []
     num.times { results << single_roll }
+    puts results.join(", ")
     results
   end
 
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,19 +1,16 @@
 require 'dice'
 
 describe Dice do
-  it 'returns a random number from 1 to 6 when rolled once' do
-    100.times do
-      r = subject.roll
-      print "#{r}-"
+  it 'returns <n> random numbers from 1 to 6 when rolled <n> times' do
+    10.times do
+      r = subject.roll(rand(20) + 1)
       r.each { |entry| expect(1..6).to include entry }
     end
   end
 
-  it 'returns <n> random numbers from 1 to 6 when rolled <n> times' do
-    10.times do
-      r = subject.roll(rand(10) + 1)
-      print "#{r}-"
-      r.each { |entry| expect(1..6).to include entry }
-    end
+  it 'prints the result to the console in the correct format' do
+    expect {subject.roll}.to output(/[1-6]/).to_stdout
+    expect {subject.roll(7)}.to output(/([1-6], ){6}[1-6]/).to_stdout
+    expect {subject.roll(21)}.to output(/([1-6], ){20}[1-6]/).to_stdout
   end
 end
