55e9e92dad7bb26e6fde23909e960fe145f00165
Al <>
2018-11-01 13:47:36 +0000

    refactored methods based on single responsibility principle

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,12 +1,17 @@
 class Dice
   def roll(num = 1)
-    results = []
-    num.times { results << single_roll }
+    results = roll_the_dice(num)
     puts results.join(", ")
     results
   end
 
   private
+
+  def roll_the_dice(num)
+    results = []
+    num.times { results << single_roll }
+    results
+  end
 
   def single_roll
     rand(6) + 1
